1c26f2d2f5c0eea3ee779cbc49a1d7bd17d2791e
andrewshields <Ashields0923@gmail.com>
2021-03-20 21:31:22 -0400

    updated migrations, models, controller, and views

---
+++ database/migrations/2021_03_20_032641_create_user_files_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_files', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("path");
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_files');
+    }
+}
