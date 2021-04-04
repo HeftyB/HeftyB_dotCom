9886c42edbb31a3fb2c7e8b0f04be62d670724d9
andrewshields <Ashields0923@gmail.com>
2021-04-03 20:54:31 -0400

    blog page layout

--- resources/views/blog_post.blade.php
+++ resources/views/blog_post.blade.php
@@ -2,15 +2,15 @@
 
 
 @section("content")
+    <div class="w-9/12 mx-auto">
+        <div class="container">
+            <img src="{{ $img }}" alt="blogImg" class="w-5/6 mx-auto rounded">
+        </div>
 
-    <div class="container">
-        <img src="{{ $img }}" alt="blogImg" class="w-5/6 mx-auto rounded">
+        <p class="text-6xl font-extrabold">{{ $title }}</p>
+
+        @foreach($elements as $element)
+            <p class="container m-2 p-4">{{ $element->value }}</p>
+        @endforeach
     </div>
-
-    <p class="text-6xl font-extrabold">{{ $title }}</p>
-
-    @foreach($elements as $element)
-        <p class="container">{{ $element->value }}</p>
-    @endforeach
-
 @endsection
