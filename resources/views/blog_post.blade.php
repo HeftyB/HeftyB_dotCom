02a309daac52efd92584292f7a80c6b74971bab7
andrewshields <Ashields0923@gmail.com>
2021-04-12 22:24:10 -0400

    blog page layout

--- resources/views/blog_post.blade.php
+++ resources/views/blog_post.blade.php
@@ -4,7 +4,7 @@
 @section("content")
     <div class="w-9/12 mx-auto">
         <div class="container">
-            <img src="{{ $img }}" alt="blogImg" class="w-5/6 mx-auto rounded">
+            <img src="/blog/{{ $img }}" alt="blogImg" class="w-5/6 mx-auto rounded">
         </div>
 
         <p class="text-6xl font-extrabold">{{ $title }}</p>
