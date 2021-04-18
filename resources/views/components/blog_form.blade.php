eebb7fb3cb7cf1f2c1db506a93c0a80c84827966
andrewshields <Ashields0923@gmail.com>
2021-04-17 23:01:10 -0400

    update blog_form.blade.php

--- resources/views/components/blog_form.blade.php
+++ resources/views/components/blog_form.blade.php
@@ -7,25 +7,63 @@
 
         <div class="w-full" id="blogContainer">
 
-            <div class="container h-36 m-auto">
+            <div class="
+            modal h-screen w-full fixed left-0 top-0 flex flex-col justify-center items-center bg-black bg-opacity-50 z-50
+{{--            container h-36 m-auto--}}
+            ">
+                <div class="container flex flex-col bg-gray-200 h-2/4 w-8/12 justify-center p-4">
+                    <p class="text-red-500 text-xs text-center">*Before you begin writing your thoughts, please take a
+                        moment to choose a title for your blog post and a background img*</p>
 
-                <p class="text-4xl text-center mx-auto cursor-text" id="blogTitle" contenteditable="true">Click to
-                    change title!</p>
+                    <hr class="my-4"/>
 
-                <div class="container text-center">
-                    <form method="post" action="" enctype="multipart/form-data" id="blogImgForm">
-                        <div class="preview">
-                            <img src="" id="blogImgMain" class="w-24 h-24 mx-auto border-black border hidden">
+                    <div class="flex justify-around w-full">
+                        <div class="w-1/2 flex align-middle justify-end pr-16 text-center text-4xl font-bold">
+                            <p class="my-auto">Choose a title:</p>
                         </div>
-                        <div>
-                            <input type="file" id="blogImgMainFile" name="file"/>
-                            <input type="button" class="button" value="Upload" id="blogImgMainUpload">
+                        <input type="text" class="shadow appearance-none border rounded py-2 px-3 mx-auto text-grey-darker w-5/12">
+{{--                        <div class="text-4xl text-center mx-auto cursor-text" id="blogTitle" contenteditable="true">Click to--}}
+{{--                            change title!</div>--}}
+                    </div>
+
+                    <div class="flex justify-around h-4/6">
+                        <div class="w-1/2 text-right flex align-middle justify-end pr-16 text-3xl font-bold border-r border-black">
+                            <p class="my-auto">Choose a cover photo:</p>
                         </div>
-                    </form>
+
+                        <div class="w-1/2 text-center">
+                            <form method="post" action="" enctype="multipart/form-data" id="blogImgForm" class="flex flex-col justify-between h-full">
+                                <div class="text-center text-xl">
+                                    Image preview:
+                                </div>
+                                <div class="preview h-full">
+                                    <img src="" id="blogImgMain" class="w-1/3 my-4 mx-auto border-black border inline-block hidden">
+                                </div>
+                                <div>
+                                    <label
+                                        for="blogImgMainFile"
+                                        class="font-semibold text-xl p-4 text-gray-600 m-4 bg-white border border-gray-400 rounded-lg text-center transition duration-200 ease-in-out transform hover:-translate-y-px shadow hover:shadow-md cursor-pointer">
+                                        Select image file
+                                    </label>
+                                    <input type="file" id="blogImgMainFile" name="file" class="hidden"/>
+                                    <input
+                                        type="button"
+                                        class="font-semibold text-xl px-4 py-3 text-gray-600 m-4 bg-white border border-gray-400 rounded-lg text-center transition duration-200 ease-in-out transform hover:-translate-y-px shadow hover:shadow-md cursor-pointer"
+                                        value="Upload"
+                                        id="blogImgMainUpload">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div
+                        class="h-16 bg-gray-800 rounded-lg border text-center shadow transition duration-200 ease-in-out transform hover:-translate-y-px hover:shadow-md cursor-pointer"
+                        id="closeTitleButton">
+                        <p class="font-semibold text-white m-3 resize">OK</p>
+                    </div>
+
                 </div>
 
-                <p class="text-red-500 text-xs text-center">*Before you begin writing your thoughts, please take a
-                    moment to choose a title for your blog post and a background img*</p>
             </div>
 
             <hr>
@@ -39,12 +77,17 @@
                 <div
                     class="bg-gray-800 rounded-lg mt-4 border text-center shadow transition duration-200 ease-in-out transform hover:-translate-y-px hover:shadow-md cursor-pointer"
                     id="cancelButton">
-                    <p class="font-semibold text-white m-3 resize">Primary Button</p>
+                    <p class="font-semibold text-white m-3 resize">Cancel</p>
+                </div>
+                <div
+                    class="bg-gray-800 rounded-lg mt-4 border text-center shadow transition duration-200 ease-in-out transform hover:-translate-y-px hover:shadow-md cursor-pointer"
+                    id="cancelButton">
+                    <p class="font-semibold text-white m-3 resize">Title/Img</p>
                 </div>
                 <div
                     id="saveButton"
                     class="text-gray-600 mt-4 bg-white border border-gray-400 rounded-lg text-center transition duration-200 ease-in-out transform hover:-translate-y-px shadow hover:shadow-md cursor-pointer">
-                    <p class="font-semibold my-3 mx-4 resize">Third Button</p>
+                    <p class="font-semibold my-3 mx-4 resize">Save</p>
                 </div>
             </div>
         </div>
